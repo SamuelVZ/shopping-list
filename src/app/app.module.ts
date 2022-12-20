1b16664f3b6e2c7b221d4a9d200a42db83da0704
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-20 11:35:32 -0800

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { RecipiesComponent } from './components/recipies/recipies.component';
+import { RecipieListComponent } from './components/recipies/recipie-list/recipie-list.component';
+import { RecipieDetailComponent } from './components/recipies/recipie-detail/recipie-detail.component';
+import { RecipieItemComponent } from './components/recipies/recipie-list/recipie-item/recipie-item.component';
+import { ShoopingListComponent } from './components/shooping-list/shooping-list.component';
+import { ShoppingEditComponent } from './components/shooping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipiesComponent,
+    RecipieListComponent,
+    RecipieDetailComponent,
+    RecipieItemComponent,
+    ShoopingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
