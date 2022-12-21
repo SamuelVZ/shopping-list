e5152d217aa3557e39791ec7f18b7fc80dd7e41a
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-21 12:19:59 -0800

    recipe array added

--- src/app/components/recipies/recipie-list/recipie-list.component.ts
+++ src/app/components/recipies/recipie-list/recipie-list.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipie } from '../recipe.model';
 
 @Component({
   selector: 'app-recipie-list',
   templateUrl: './recipie-list.component.html',
-  styleUrls: ['./recipie-list.component.css']
+  styleUrls: ['./recipie-list.component.css'],
 })
 export class RecipieListComponent implements OnInit {
+  recipies: Recipie[] = [
+    {
+      name: 'test',
+      description: 'this is a test',
+      imagePath:
+        'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272',
+    },
+    {
+      name: 'test 2',
+      description: 'this is a test',
+      imagePath:
+        'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
