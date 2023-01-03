ad65ed5b906cfb845164bd819f6910c62e6dc430
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-01-03 12:37:41 -0800

    "navigation" with ng if

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
+  @Output() selected = new EventEmitter<string>();
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSelect(selection: string) {
+    this.selected.emit(selection);
+  }
 }
