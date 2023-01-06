84d07b06edd4f7fb4517dbb105c62a4d68c2af22
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-01-06 09:01:54 -0800

    form outputs the ingredient

--- src/app/components/shooping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/components/shooping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+import { Form, FormGroup } from '@angular/forms';
+import { Ingredient } from '../../../shared/models/Ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef!: ElementRef;
+  @ViewChild('amountInput') amountInputRef!: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onAddItem() {
+    const newIngredient: Ingredient = {
+      name: this.nameInputRef.nativeElement.value,
+      amount: this.amountInputRef.nativeElement.value,
+    };
+    this.ingredientAdded.emit(newIngredient);
   }
-
 }
