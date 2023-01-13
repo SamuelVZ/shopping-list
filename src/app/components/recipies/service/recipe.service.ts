e23e7c6e9e266f9c2dbdfec4f8ddd0d8d81e4cf5
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-01-13 09:30:57 -0800

    recipes array for public state & event emitter for recipe selected

--- src/app/components/recipies/service/recipe.service.ts
+++ src/app/components/recipies/service/recipe.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Recipie } from '../recipe.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeService {
+  private recipies: Recipie[] = [
+    {
+      name: 'test',
+      description: 'this is a test',
+      imagePath:
+        'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272',
+    },
+    {
+      name: 'test 2',
+      description: 'this is a second test',
+      imagePath:
+        'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272',
+    },
+  ];
+  recipeSelected = new EventEmitter<Recipie>();
 
-  constructor() { }
+  constructor() {}
+
+  getRecipes(): Recipie[] {
+    return this.recipies.slice();
+  }
 }
