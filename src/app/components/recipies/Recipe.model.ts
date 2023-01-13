2c82afa96327e517f34337f42eced7ea4c7a5e01
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-01-13 11:42:27 -0800

    recipe model now includes ingredients

--- src/app/components/recipies/Recipe.model.ts
+++ src/app/components/recipies/Recipe.model.ts
@@ -1,5 +1,7 @@
+import { Ingredient } from '../../shared/models/Ingredient.model';
 export interface Recipie {
   name: string;
   description: string;
   imagePath: string;
+  ingredients: Ingredient[];
 }
