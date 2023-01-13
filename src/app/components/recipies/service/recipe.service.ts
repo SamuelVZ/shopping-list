9dce79edab8047bbf53699c600274b1e3e567267
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-01-13 11:43:47 -0800

    recipe array now includes ingredients

--- src/app/components/recipies/service/recipe.service.ts
+++ src/app/components/recipies/service/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipie } from '../recipe.model';
+import { Ingredient } from '../../../shared/models/Ingredient.model';
 
 @Injectable({
   providedIn: 'root',
@@ -7,16 +8,24 @@ import { Recipie } from '../recipe.model';
 export class RecipeService {
   private recipies: Recipie[] = [
     {
-      name: 'test',
-      description: 'this is a test',
+      name: 'Tasty Schnitzel',
+      description: 'A super tasty schnitzel',
       imagePath:
-        'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG/1200px-Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+      ingredients: [
+        { name: 'meat', amount: 1 },
+        { name: 'French Fries', amount: 20 },
+      ],
     },
     {
-      name: 'test 2',
-      description: 'this is a second test',
+      name: 'Hamburguer',
+      description: 'An awesome Hamburger',
       imagePath:
-        'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272',
+        'https://assets.unileversolutions.com/recipes-v2/230411.jpg?imwidth=1200',
+      ingredients: [
+        { name: 'meat', amount: 1 },
+        { name: 'Buns', amount: 2 },
+      ],
     },
   ];
   recipeSelected = new EventEmitter<Recipie>();
