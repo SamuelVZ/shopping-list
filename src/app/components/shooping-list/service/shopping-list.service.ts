acace9024243a000e197eec43cc1eaaeb768cea4
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-01-13 10:05:44 -0800

    methods & emitter added

--- src/app/components/shooping-list/service/shopping-list.service.ts
+++ src/app/components/shooping-list/service/shopping-list.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Ingredient } from 'src/app/shared/models/Ingredient.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    { name: 'Apples', amount: 5 },
+    { name: 'Tomatoes', amount: 10 },
+  ];
 
-  constructor() { }
+  constructor() {}
+
+  getIngredients(): Ingredient[] {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(newIngredient: Ingredient) {
+    this.ingredients.push(newIngredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
