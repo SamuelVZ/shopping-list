c25274b1e5045df55e32a858c3ef5c02929a206e
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-01-18 11:18:43 -0800

    new paths for edit and new recipes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RecipiesComponent } from './components/recipies/recipies.component';
 import { ShoopingListComponent } from './components/shooping-list/shooping-list.component';
 import { RecipieDetailComponent } from './components/recipies/recipie-detail/recipie-detail.component';
 import { RecipeStartComponent } from './components/recipies/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './components/recipies/recipe-edit/recipe-edit.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -13,7 +14,9 @@ const routes: Routes = [
     component: RecipiesComponent,
     children: [
       { path: '', component: RecipeStartComponent },
+      { path: 'new', component: RecipeEditComponent }, //put it before the path with a paramether :id
       { path: ':id', component: RecipieDetailComponent },
+      { path: ':id/edit', component: RecipeEditComponent },
     ],
   },
   { path: 'shopping-list', component: ShoopingListComponent },
