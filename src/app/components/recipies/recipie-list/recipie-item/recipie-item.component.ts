7ce6111b288036b5521b32cda27ce2547216b59c
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-01-18 10:55:55 -0800

    recipe item recibes the id of the recipe to navigate

--- src/app/components/recipies/recipie-list/recipie-item/recipie-item.component.ts
+++ src/app/components/recipies/recipie-list/recipie-item/recipie-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipie } from '../../recipe.model';
 import { RecipeService } from '../../service/recipe.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipie-item',
@@ -9,12 +10,17 @@ import { RecipeService } from '../../service/recipe.service';
 })
 export class RecipieItemComponent implements OnInit {
   @Input() recipe!: Recipie;
+  @Input() id!: number;
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(
+    private recipeService: RecipeService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {}
 
   onSelectRecipe() {
-    this.recipeService.recipeSelected.emit(this.recipe);
+    this.router.navigate([this.id], { relativeTo: this.route });
   }
 }
