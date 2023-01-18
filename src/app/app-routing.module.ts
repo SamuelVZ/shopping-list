695e27b242bff2f3481f97df99ff3bc8962fbc26
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-01-18 10:53:45 -0800

    add children array path to recipes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,19 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { RecipiesComponent } from './components/recipies/recipies.component';
 import { ShoopingListComponent } from './components/shooping-list/shooping-list.component';
+import { RecipieDetailComponent } from './components/recipies/recipie-detail/recipie-detail.component';
+import { RecipeStartComponent } from './components/recipies/recipe-start/recipe-start.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipiesComponent },
+  {
+    path: 'recipes',
+    component: RecipiesComponent,
+    children: [
+      { path: '', component: RecipeStartComponent },
+      { path: ':id', component: RecipieDetailComponent },
+    ],
+  },
   { path: 'shopping-list', component: ShoopingListComponent },
 ];
 
