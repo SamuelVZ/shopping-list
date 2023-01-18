371738c49bc835fa566eca533a1bca343e2dd846
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-01-18 07:29:04 -0800

    a tags now use routing navigation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,13 +8,16 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
-  @Output() selected = new EventEmitter<string>();
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
 
   onSelect(selection: string) {
-    this.selected.emit(selection);
+    if (selection === 'recipes') {
+      this.router.navigate(['/recipes']);
+    } else {
+      this.router.navigate(['/shopping-list']);
+    }
   }
 }
