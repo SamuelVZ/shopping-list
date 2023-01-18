ac1b7fa2456d8aa3c7bcdada931ae4b8e4688948
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-01-18 11:32:33 -0800

    add navigation no new route

--- src/app/components/recipies/recipie-list/recipie-list.component.ts
+++ src/app/components/recipies/recipie-list/recipie-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipie } from '../recipe.model';
 import { RecipeService } from '../service/recipe.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipie-list',
@@ -10,9 +11,17 @@ import { RecipeService } from '../service/recipe.service';
 export class RecipieListComponent implements OnInit {
   recipes: Recipie[] = [];
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(
+    private recipeService: RecipeService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
+  }
+
+  onNewRecipe() {
+    this.router.navigate(['new'], { relativeTo: this.route });
   }
 }
