a4e8380c47e5f9bebde165ae99f47c92f7deb726
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-01-20 10:39:49 -0800

    remove unussed code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  selection: string = 'recipe';
-  title = 'shopping-list';
-
-  onSelection(e: string) {
-    this.selection = e;
-  }
-}
+export class AppComponent {}
