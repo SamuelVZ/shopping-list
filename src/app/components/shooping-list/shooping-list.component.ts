120d878bcd69050e380611eaa5dc8409f71a09e2
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-01-20 10:38:02 -0800

    add a subscription to unsubscribe at the end

--- src/app/components/shooping-list/shooping-list.component.ts
+++ src/app/components/shooping-list/shooping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../../shared/models/Ingredient.model';
 import { ShoppingListService } from './service/shopping-list.service';
 
@@ -7,15 +8,20 @@ import { ShoppingListService } from './service/shopping-list.service';
   templateUrl: './shooping-list.component.html',
   styleUrls: ['./shooping-list.component.css'],
 })
-export class ShoopingListComponent implements OnInit {
+export class ShoopingListComponent implements OnInit, OnDestroy {
   ingredients!: Ingredient[];
+  private ingChangedSub!: Subscription;
 
   constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit(): void {
     this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientsChanged.subscribe(
+    this.ingChangedSub = this.shoppingListService.ingredientsChanged.subscribe(
       (newIngredients) => (this.ingredients = newIngredients)
     );
+  }
+
+  ngOnDestroy(): void {
+    this.ingChangedSub.unsubscribe();
   }
 }
