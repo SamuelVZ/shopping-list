baf4899d1ee5ef682a0e5ab0373cb9d0a3bffbf6
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-02-01 12:08:19 -0800

    subscription added to edit the item selected, on destroy to unsubscribe

--- src/app/components/shooping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/components/shooping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../../../shared/models/Ingredient.model';
 import { ShoppingListService } from '../service/shopping-list.service';
 
@@ -13,8 +14,11 @@ import { ShoppingListService } from '../service/shopping-list.service';
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css'],
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
   shoppingListForm!: FormGroup;
+  subscription!: Subscription;
+  editMode = false;
+  editItemIndex!: number;
 
   constructor(
     private shoppingListService: ShoppingListService,
@@ -22,6 +26,22 @@ export class ShoppingEditComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.buildForm();
+
+    this.subscription = this.shoppingListService.startedEditing.subscribe(
+      (index) => {
+        this.editMode = true;
+        this.editItemIndex = index;
+        // this.shoppingListForm;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  buildForm() {
     this.shoppingListForm = this.fb.group({
       name: new FormControl('', Validators.required),
       amount: new FormControl('', [
