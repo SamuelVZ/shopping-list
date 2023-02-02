b7767c07fac35983c92dd07c30a294044e61bc48
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-02-02 07:29:10 -0800

    initialize the form

--- src/app/components/recipies/recipe-edit/recipe-edit.component.ts
+++ src/app/components/recipies/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { RecipeService } from '../service/recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,13 +11,42 @@ import { ActivatedRoute } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id!: number;
   editMode = false;
+  recipeForm!: FormGroup;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private recipeService: RecipeService
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       this.id = +params['id'];
       this.editMode = params['id'] != null;
+      this.initForm();
+    });
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
+  private initForm() {
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipeById(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+    }
+
+    this.recipeForm = this.fb.group({
+      name: [recipeName, Validators.required],
+      imagePath: [recipeImagePath, Validators.required],
+      description: [recipeDescription, Validators.required],
     });
   }
 }
