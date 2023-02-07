6fbb7df4b7ea63c8616b19817659bd2f612d2d0f
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-02-07 11:46:58 -0800

    add check to the resolver

--- src/app/components/recipies/recipes.resolver.ts
+++ src/app/components/recipies/recipes.resolver.ts
@@ -8,18 +8,28 @@ import {
 import { Observable, of } from 'rxjs';
 import { Recipie } from './recipe.model';
 import { DataStorageService } from '../../shared/data-storage.service';
+import { RecipeService } from './service/recipe.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecipesResolver implements Resolve<Recipie[]> {
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(
+    private dataStorageService: DataStorageService,
+    private recipeService: RecipeService
+  ) {}
 
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): Observable<Recipie[]> {
-    //not subscribing here cause the resolver will subscribe automatically
-    return this.dataStorageService.fetchRecipes();
+  ): Observable<Recipie[]> | Recipie[] {
+    const recipes = this.recipeService.getRecipes();
+
+    if (recipes.length === 0) {
+      //not subscribing here cause the resolver will subscribe automatically
+      return this.dataStorageService.fetchRecipes();
+    } else {
+      return recipes;
+    }
   }
 }
