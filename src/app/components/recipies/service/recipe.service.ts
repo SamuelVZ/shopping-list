23c3a4ed874613f4d9d59bae570570baaffdcbb4
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-02-07 09:49:47 -0800

    set recipes method added, no longer using preloaded data

--- src/app/components/recipies/service/recipe.service.ts
+++ src/app/components/recipies/service/recipe.service.ts
@@ -7,30 +7,35 @@ import { Subject } from 'rxjs';
 })
 export class RecipeService {
   recipesChanged = new Subject<Recipie[]>();
-  private recipes: Recipie[] = [
-    {
-      name: 'Tasty Schnitzel',
-      description: 'A super tasty schnitzel',
-      imagePath:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG/1200px-Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
-      ingredients: [
-        { name: 'meat', amount: 1 },
-        { name: 'French Fries', amount: 20 },
-      ],
-    },
-    {
-      name: 'Hamburguer',
-      description: 'An awesome Hamburger',
-      imagePath:
-        'https://assets.unileversolutions.com/recipes-v2/230411.jpg?imwidth=1200',
-      ingredients: [
-        { name: 'meat', amount: 1 },
-        { name: 'Buns', amount: 2 },
-      ],
-    },
-  ];
+  private recipes: Recipie[] = [];
+  //   {
+  //     name: 'Tasty Schnitzel',
+  //     description: 'A super tasty schnitzel',
+  //     imagePath:
+  //       'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG/1200px-Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+  //     ingredients: [
+  //       { name: 'meat', amount: 1 },
+  //       { name: 'French Fries', amount: 20 },
+  //     ],
+  //   },
+  //   {
+  //     name: 'Hamburguer',
+  //     description: 'An awesome Hamburger',
+  //     imagePath:
+  //       'https://assets.unileversolutions.com/recipes-v2/230411.jpg?imwidth=1200',
+  //     ingredients: [
+  //       { name: 'meat', amount: 1 },
+  //       { name: 'Buns', amount: 2 },
+  //     ],
+  //   },
+  // ];
 
   constructor() {}
+
+  setRecipes(recipes: Recipie[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes(): Recipie[] {
     return this.recipes.slice();
