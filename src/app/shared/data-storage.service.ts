95186d35490b2175470249aa3d63daf2a73021cc
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-02-08 14:51:22 -0800

    subscribe to user & use exhaustMap to return the request observable

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,14 +1,19 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, tap } from 'rxjs';
+import { exhaustMap, map, take, tap } from 'rxjs';
 import { Recipie } from '../components/recipies/recipe.model';
 import { RecipeService } from '../components/recipies/service/recipe.service';
+import { AuthService } from '../components/auth/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
@@ -25,23 +30,30 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.http
-      .get<Recipie[]>(
-        'https://recipe-book-e2dc3-default-rtdb.firebaseio.com/recipes.json',
-        { observe: 'body' }
-      )
-      .pipe(
-        map((recipies) => {
-          return recipies.map((recipe) => {
-            return {
-              ...recipe,
-              ingredients: recipe.ingredients ? recipe.ingredients : [],
-            };
-          });
-        }),
-        tap((recipes) => {
-          this.recipeService.setRecipes(recipes);
-        })
-      );
+    return this.authService.user.pipe(
+      take(1), //to take one value from the observable and then unsubscribe
+      exhaustMap((user) => {
+        //waits for the fist observable to complete (authService.user), then return a new obervable that replaces the previous one
+        let userToken: string = '';
+        if (!!user) {
+          userToken = user.token;
+        }
+        return this.http.get<Recipie[]>(
+          'https://recipe-book-e2dc3-default-rtdb.firebaseio.com/recipes.json',
+          { params: new HttpParams().set('auth', userToken), observe: 'body' }
+        );
+      }),
+      map((recipies) => {
+        return recipies.map((recipe) => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : [],
+          };
+        });
+      }),
+      tap((recipes) => {
+        this.recipeService.setRecipes(recipes);
+      })
+    );
   }
 }
