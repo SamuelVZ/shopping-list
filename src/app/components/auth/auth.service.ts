a653ddae466a0477a8e906bdb8cff0164b3c81d8
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-02-08 12:03:34 -0800

    responsedata interface modified for login, login method & handleError methods added

--- src/app/components/auth/auth.service.ts
+++ src/app/components/auth/auth.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+
+  //extra fields for login
+  displayName?: string;
+  registered?: string;
 }
 
 @Injectable({
@@ -16,13 +21,53 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCdAXjAhgHHOy8SkxH1JoyOizMOjXiYoYw',
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true,
-      }
-    );
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCdAXjAhgHHOy8SkxH1JoyOizMOjXiYoYw',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(catchError(this.handleError));
+    //catch error checks for the errors and the return if an error ocurred;
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCdAXjAhgHHOy8SkxH1JoyOizMOjXiYoYw',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorResponse: HttpErrorResponse) {
+    let errorMsg = 'An unknown error ocurred';
+
+    if (!errorResponse.error || !errorResponse.error.error) {
+      return throwError(() => new Error(errorMsg));
+    }
+
+    switch (errorResponse.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMsg = 'This email already exists';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMsg = 'This email does not exist';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMsg = 'Incorrect password';
+        break;
+      case 'USER_DISABLED':
+        errorMsg = 'The user account has been disabled by an administrator';
+        break;
+    }
+    return throwError(() => new Error(errorMsg));
   }
 }
