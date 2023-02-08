1e72e93452cecd688e331adc86565e80cb2963c2
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-02-08 14:52:57 -0800

    subscribe to user to know if a user is loggedIn

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,21 +1,40 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+  OnDestroy,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { DataStorageService } from '../../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   collapsed = true;
+  isLoggedIn = false;
+  userSubscription!: Subscription;
 
   constructor(
     private router: Router,
-    private dataStorageService: DataStorageService
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.userSubscription = this.authService.user.subscribe((user) => {
+      this.isLoggedIn = !!user;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
 
   onSelect(selection: string) {
     if (selection === 'recipes') {
