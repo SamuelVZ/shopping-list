3babcd163654f223ac4c961589ee5736da66c338
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-02-10 14:38:45 -0800

    AuthModule added to imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthComponent } from './components/auth/auth.component';
 import { AuthInterceptor } from './components/auth/auth.interceptor';
 import { RecipesModule } from './components/recipies/recipes.module';
 import { ShoppingListModule } from './components/shooping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './components/auth/auth.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, AuthComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
     SharedModule,
+    AuthModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
