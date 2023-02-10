f8a83f7c6e1da44968b2ecb92beb6b394ba4ef3c
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-02-10 14:22:45 -0800

    added SharedModule, remove declarations & unussed imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { ShoopingListComponent } from './components/shooping-list/shooping-list.component';
-import { ShoppingEditComponent } from './components/shooping-list/shopping-edit/shopping-edit.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DropdownDirective } from './shared/dropdown.directive';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthComponent } from './components/auth/auth.component';
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptor } from './components/auth/auth.interceptor';
-import { AlertComponent } from './shared/alert/alert.component';
 import { RecipesModule } from './components/recipies/recipes.module';
 import { ShoppingListModule } from './components/shooping-list/shopping-list.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    DropdownDirective,
-    AuthComponent,
-    LoadingSpinnerComponent,
-    AlertComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent, AuthComponent],
   imports: [
     BrowserModule,
     FormsModule,
@@ -33,6 +21,7 @@ import { ShoppingListModule } from './components/shooping-list/shopping-list.mod
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
+    SharedModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
