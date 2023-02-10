e70a4dd84ddc88e226fc09fb4e0b67a71ebc14a8
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-02-10 09:55:35 -0800

    RecipesRoutingModule created with routes

---
+++ src/app/components/recipies/recipes-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { RecipiesComponent } from './recipies.component';
+import { AuthGuard } from '../auth/auth.guard';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipesResolver } from './recipes.resolver';
+import { RecipieDetailComponent } from './recipie-detail/recipie-detail.component';
+
+const routes: Routes = [
+  {
+    path: 'recipes',
+    component: RecipiesComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: '', component: RecipeStartComponent },
+      { path: 'new', component: RecipeEditComponent }, //put it before the path with a paramether :id
+      {
+        path: ':id',
+        component: RecipieDetailComponent,
+        resolve: [RecipesResolver],
+      },
+      {
+        path: ':id/edit',
+        component: RecipeEditComponent,
+        resolve: [RecipesResolver],
+      },
+    ],
+  },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class RecipesRoutingModule {}
