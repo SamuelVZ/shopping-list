0b2e5bd8342c4bf6eb37e20b6c86292d8367075b
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-02-10 09:56:04 -0800

    moved recipes routes and imports to RecipesRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { RecipiesComponent } from './components/recipies/recipies.component';
 import { ShoopingListComponent } from './components/shooping-list/shooping-list.component';
-import { RecipieDetailComponent } from './components/recipies/recipie-detail/recipie-detail.component';
-import { RecipeStartComponent } from './components/recipies/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './components/recipies/recipe-edit/recipe-edit.component';
-import { RecipesResolver } from './components/recipies/recipes.resolver';
+
 import { AuthComponent } from './components/auth/auth.component';
-import { AuthGuard } from './components/auth/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  {
-    path: 'recipes',
-    component: RecipiesComponent,
-    canActivate: [AuthGuard],
-    children: [
-      { path: '', component: RecipeStartComponent },
-      { path: 'new', component: RecipeEditComponent }, //put it before the path with a paramether :id
-      {
-        path: ':id',
-        component: RecipieDetailComponent,
-        resolve: [RecipesResolver],
-      },
-      {
-        path: ':id/edit',
-        component: RecipeEditComponent,
-        resolve: [RecipesResolver],
-      },
-    ],
-  },
   { path: 'shopping-list', component: ShoopingListComponent },
   { path: 'auth', component: AuthComponent },
 ];
