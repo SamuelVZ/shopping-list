ed1d0c71de487f6c637c045fd97493dc546ccf92
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-02-14 14:15:27 -0800

    refractor ingredients, reduces to addIngredients action added

--- src/app/components/shooping-list/state/shopping-list.reducer.ts
+++ src/app/components/shooping-list/state/shopping-list.reducer.ts
@@ -2,17 +2,22 @@ import { Action, createReducer, on } from '@ngrx/store';
 import * as fromActions from './shopping-list.actions';
 import { Ingredient } from 'src/app/shared/models/Ingredient.model';
 
+const ingredients: Ingredient[] = [
+  { name: 'Apples', amount: 5 },
+  { name: 'Tomatoes', amount: 10 },
+];
+
 const initialState = {
-  ingredients: [
-    { name: 'Apples', amount: 5 },
-    { name: 'Tomatoes', amount: 10 },
-  ],
+  ingredients: ingredients,
 };
 
 export const shoppingListReducer = createReducer(
   initialState,
   on(fromActions.addIngredient, (state, { ingredient }) => {
     return { ...state, ingredients: [...state.ingredients, ingredient] };
+  }),
+  on(fromActions.addIngredients, (state, { ingredients }) => {
+    return { ...state, ingredients: [...state.ingredients, ...ingredients] };
   })
 );
 
