bf5fe18ecc4326f6bd402d4aa1d183aef8491df7
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2023-02-14 14:50:14 -0800

    updated & delete Ingredient actions added

--- src/app/components/shooping-list/state/shopping-list.actions.ts
+++ src/app/components/shooping-list/state/shopping-list.actions.ts
@@ -2,10 +2,21 @@ import { createAction, props } from '@ngrx/store';
 import { Ingredient } from '../../../shared/models/Ingredient.model';
 
 export const addIngredient = createAction(
-  '[Shopping-list] Add Ingredient to the list',
+  '[Shopping-list] Add Ingredient to the store',
   props<{ ingredient: Ingredient }>()
 );
+
 export const addIngredients = createAction(
-  '[Shopping-list] Add multiple Ingredients to the list',
+  '[Shopping-list] Add multiple Ingredients to the store',
   props<{ ingredients: Ingredient[] }>()
+);
+
+export const updatedIngredient = createAction(
+  '[Shopping-list] Update an Ingredient to the store',
+  props<{ index: number; ingredient: Ingredient }>()
+);
+
+export const deleteIngredient = createAction(
+  '[Shopping-list] delete an Ingredient to the store',
+  props<{ index: number }>()
 );
